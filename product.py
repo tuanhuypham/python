ad615d4245f2edbd0cd323ece2b7bd74263e95a6
huypham <huy5153a@gmail.com>
2021-10-14 10:29:17 +0700

    update

--- product.py
+++ product.py
@@ -1,14 +1,18 @@
-
-
-
-
-
-
-
-
 # from itertools import product
 
 # a = list(map(int, input().split()))
 # b = list(map(int, input().split()))
 
 # print(*product(a,b))
+
+
+
+class Pro():
+    pass
+from itertools import product
+
+PDT = Pro()
+
+PDT.a = list(map(int, input().split()))
+PDT.b = list(map(int, input().split()))
+PDT.C = print(*product(PDT.a,PDT.b))
